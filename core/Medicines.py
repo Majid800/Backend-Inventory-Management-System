94709b1cae79c74ae5cb707d58eec381512a794a
Majid800 <majid887799@outlook.com>
2026-06-22 15:17:42 +0100

    fix medicine class initialisation errors

--- core/Medicines.py
+++ core/Medicines.py
@@ -1,27 +1,26 @@
 #First stage is the creation of the Medicine Database. This file acts as the foundation of the entire system
 
 from dataclasses import dataclass, field 
-from typing import list, Dict, Optional
+from typing import List, Dict, Optional
 
 #creating medicine class and assigning attributes
 @dataclass
 class Medicine:
     name: str
     strength: str
-    formuation: str 
+    formulation: str 
     tablets_per_pack: int 
     manufacturer: str 
-    is_controlled:bool = False
     code: str 
     barcode: str =""
     notes: str = "" 
+    is_controlled:bool = False
 
     #Cleaning the data provided from user 
     def __post_init__(self):
         self.name = self.name.strip()
         self.strength = self.strength.strip()
         self.formulation = self.formulation.strip()
-        self.tablets_per_pack = self.tablets_per_pack.strip()
         self.code = self.code.strip().upper()
         self.barcode = self.barcode.strip()
         self.manufacturer.strip()
@@ -41,7 +40,10 @@ class Medicine:
             f"Tablets per pack: {self.tablets_per_pack}\n"
             f"Code: {self.code}\n"
             f"Barcode: {self.barcode or 'N/A'}\n"
-            f"Manufacturer: {self.manufacturer or 'N\A'}\n"
+            f"Manufacturer: {self.manufacturer or 'N/A'}\n"
             f"Notes: {self.notes or 'N/A'}\n"
         )
         
+
+med1 = Medicine(name = "Amlodipine", strength = "10mg", formulation = "tablets", tablets_per_pack= 28, is_controlled= False, manufacturer = "Teva", code = "AML10TAB")
+print(med1)
