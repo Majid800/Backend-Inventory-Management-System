bf18091193a548852e4b80b6e65bdcc74fa9cfd4
Majid800 <majid887799@outlook.com>
2026-06-23 23:56:04 +0100

    Add PostgreSQL database integration

--- core/database.py
+++ core/database.py
@@ -1,3 +1,4 @@
+from medicines import Medicine
 import psycopg 
 def connect():
     conn = psycopg.connect(host ="localhost", dbname ="pharmacy_inventory", user="postgres", password ="King1978!")
@@ -18,5 +19,77 @@ def get_all_medicines():
     cur.close()
     conn.close()
 
+def add_medicine(medicine):
+        conn = psycopg.connect(
+        host="localhost",
+        dbname="pharmacy_inventory",
+        user="postgres",
+        password="King1978"
+    )
+        cur = conn.cursor()
+        cur.execute(
+        """
+        INSERT INTO medicines
+        (name, strength, formulation, code, barcode, manufacturer, pack_size, notes, is_controlled)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+            medicine.name,
+            medicine.strength,
+            medicine.formulation,
+            medicine.code,
+            medicine.barcode,
+            medicine.manufacturer,
+            medicine.pack_size,
+            medicine.notes,
+            medicine.is_controlled
+        )
+    )
+
+    
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+def search_by_code(code):
+    conn = psycopg.connect(
+        host="localhost",
+        dbname="pharmacy_inventory",
+        user="postgres",
+        password="King1978!"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM medicines WHERE code = %s",
+        (code,)
+    )
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    return row
+
+def delete_by_code(code):
+    conn = psycopg.connect(
+        host="localhost",
+        dbname="pharmacy_inventory",
+        user="postgres",
+        password="King1978"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "DELETE FROM medicines WHERE code = %s",
+        (code,)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
+
+
+
 if __name__ == "__main__":
-    get_all_medicines()
+    result = search_by_code("PAR500TAB")
+    print(result)
